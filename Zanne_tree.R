65c8237299432a890f7206a118d66e4f621277b5
mac user <macuser@dhcp-206-87-131-51.ubcsecure.wireless.ubc.ca>
2022-05-13 15:00:38 -0700

    Made standalone, so can be initiated first. Removed useless comment.

--- Zanne_tree.R
+++ Zanne_tree.R
@@ -1,9 +1,17 @@
 library(taxize)
 library(ape)
 
+zannetree <- read.tree("Zanne.angiosperm.tre")
+zannefreeze <- read.csv("MinimumFreezingExposure.csv")
+zannetree$tip.label <- trimws(tolower(zannetree$tip.label),"both")
+zannefreeze$species <- trimws(gsub(" ","_",tolower(zannefreeze$species)),"both")
+nofreezenames <- which(is.na(match(zannetree$tip.label,zannefreeze$species)))
+myfreeze <- zannefreeze[which(!is.na(match(zannefreeze$species,zannetree$tip.label))),]
+zannetree.trim <- drop.tip(zannetree,nofreezenames)
+
 tip.phyla <- data.frame(species=zannetree.trim$tip.label, phylum = NA)
 tip.phyla[6814,2] <- "Magnoliopsida" #DB lookup issue for this species
-for(i in 6815:nrow(tip.phyla)){#(1:nrow(tip.phyla))[-6814]){
+for(i in 6815:nrow(tip.phyla)){
   tip.phyla$phylum[i] <- classification(tip.phyla$species[i], db="gbif", rows = 1)[[1]][3,1]
 }
 
